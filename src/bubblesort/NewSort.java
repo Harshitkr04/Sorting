153f2f4625ba2c39120819a535853f140e4fad6d
harshitkumar <harshitkumar@localhost>
2022-09-06 15:24:03 +0530

    New Sort

--- src/bubblesort/NewSort.java
+++ src/bubblesort/NewSort.java
@@ -1,42 +1,33 @@
-package sorting;
+package bubblesort;
 
 public class NewSort {
-	 void sort(int arr[])
-	    {
-	        int n = arr.length;
-	        for (int i = 1; i < n; ++i) {
-	            int key = arr[i];
-	            int j = i - 1;
-	  
-	            /* Move elements of arr[0..i-1], that are
-	               greater than key, to one position ahead
-	               of their current position */
-	            while (j >= 0 && arr[j] > key) {
-	                arr[j + 1] = arr[j];
-	                j = j - 1;
-	            }
-	            arr[j + 1] = key;
-	        }
-	    }
-	  
-	    /* A utility function to print array of size n*/
-	    static void printArray(int arr[])
-	    {
-	        int n = arr.length;
-	        for (int i = 0; i < n; ++i)
-	            System.out.print(arr[i] + " ");
-	  
-	        System.out.println();
-	    }
-	  
-	    // Driver method
-	    public static void main(String args[])
-	    {
-	        int arr[] = { 12, 11, 13, 5, 6 };
-	  
-	        NewSort ob = new NewSort();
-	        ob.sort(arr);
-	  
-	        printArray(arr);
-	    }
-	}
+	public static void insertionSort(int array[]) {  
+        int n = array.length;  
+        for (int j = 1; j < n; j++) {  
+            int key = array[j];  
+            int i = j-1;  
+            while ( (i > -1) && ( array [i] > key ) ) {  
+                array [i+1] = array [i];  
+                i--;  
+            }  
+            array[i+1] = key;  
+        }  
+    }  
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr1 = {9,14,3,2,43,11,58,22};    
+        System.out.println("Before Insertion Sort");    
+        for(int i:arr1){    
+            System.out.print(i+" ");    
+        }    
+        System.out.println();    
+            
+        insertionSort(arr1);//sorting array using insertion sort    
+           
+        System.out.println("After Insertion Sort");    
+        for(int i:arr1){    
+            System.out.print(i+" ");    
+        }    
+    }    
+}
